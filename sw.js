586d62059b9e085be437a4922b436b593879b714
Shiva E S <77491431+itsshivaes@users.noreply.github.com>
2026-04-18 11:18:50 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,15 @@
-const CACHE_NAME='lantro-pwa-v1',urlsToCache=['/','/search'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE_NAME = 'lantro-pwa-v1';
+const urlsToCache = ['/', '/search'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
