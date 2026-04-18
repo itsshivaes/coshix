529c86bcb20c4d43e0b3ad6219e60799b4c06da1
Shiva E S <77491431+itsshivaes@users.noreply.github.com>
2026-04-18 08:33:58 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE_NAME='lantro-pwa-v1',urlsToCache=['/','/search'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
